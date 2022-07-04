c50a58f3630d90162f468a442d6632ee734c8744
ginimabry <103955855+ginimabry@users.noreply.github.com>
2022-07-04 14:01:59 -0500

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 function displayTemperature(response) {
   console.log(response.data);
-}
 
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 let apiKey = "74fb52063cc6d812d7470c563e6befc0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
-axious.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
